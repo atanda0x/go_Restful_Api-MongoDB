fc4f33d0f5abb4edbc6ca48a88b23d008f3f66d4
atanda0x <atandanafiu21@gmail.com>
2023-06-13 23:51:24 +0100

    Project completed

---
+++ controllers/user.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/atanda0x/go_Restful_Api-MongoDB/models"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	old := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	if err := uc.session.DB("go_Restful_Api-MongoDB").C("users").FindId(old).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = bson.NewObjectId()
+	uc.session.DB("go_Restful_Api-MongoDB").C("users").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	old := bson.ObjectIdHex(id)
+	if err := uc.session.DB("go_Restful_Api-MongoDB").C("users").Remove(old); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	w.WriteHeader(200)
+	fmt.Fprint(w, "Delete user", old, "\n")
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/atanda0x/go_Restful_Api-MongoDB
 
 go 1.20
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
